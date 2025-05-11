4b24a472d64e6be2c786bd5bfd2c729a19c10b10
D-Unit <duane@dtechmediacreations.com>
2025-05-11 02:08:11 -0500

    Fix: Enable IRQ0 timer handler

--- kernel/arch/x86_64/timer.c
+++ kernel/arch/x86_64/timer.c
@@ -98,7 +98,6 @@ void timer_init(void) {
     outb(PIT_CHANNEL0, (reload_value >> 8) & 0xFF);
     
     // Register the timer handler for IRQ0
-    extern void register_interrupt_handler(uint8_t, interrupt_handler_t);
     register_interrupt_handler(32, (interrupt_handler_t)timer_handler);
     
     // Unmask (enable) IRQ0 in the PIC
